7aaef1be585db7de6cc0d43fabd4342aaea967fb
Amirreza Bahoush <a.bahoush@fwutech.com>
2021-02-27 13:11:07 +0330

    complete add functionality

--- src/App.js
+++ src/App.js
@@ -15,12 +15,12 @@ const App = () => {
   const handleSearch = (value) => {
     const values = value.split(' ');
     const currentTypedValue = values[values.length - 1];
-    if(currentTypedValue.includes('#')) {
+    if (currentTypedValue.includes('#')) {
       const typedText = currentTypedValue.slice(1);
-      api('tag', 'GET', null, { type: 'tag', tag: typedText}).then(response => {
+      api('tag', 'GET', null, { type: 'tag', tag: typedText }).then(response => {
         const options = [];
         response.data.list.map(item => {
-          options.push({value: parse(item)});
+          options.push({ value: parse(item) });
         })
         setOptions(options);
       })
@@ -28,47 +28,94 @@ const App = () => {
   };
 
   const onSelect = (value) => {
-    const selectedValue = `${value[0].props.children}${value[1]}`
-    console.log('onSelect', selectedValue);
-    message.info(`your selected item is: ${selectedValue}`)
+    const selectedVal = `${value[0].props.children}${value[1]}`
+    message.info(`your selected item is: ${selectedVal}`)
   };
 
   const onChange = value => {
-    if(Array.isArray(value)) {
-    debugger
-    console.log(value, "value");
-      const selectedValue = `${value[0].props.children}${value[1]}`;
-      setSelectedValue(prevValue => `${prevValue} ${selectedValue}`);
-    }else{
+    if (Array.isArray(value)) {
+      console.log(value, "value");
+      const lettersArray = [];
+      value.filter((item, index) => {
+        typeof item === 'object' &&
+          lettersArray.push(index);
+        return '';
+      }
+      );
+      let selectedVal;
+      lettersArray.map(item => {
+        switch (item) {
+          case 0:
+            selectedVal = `${value[0].props.children}${value[1]}`;
+            break;
+          case 1:
+            selectedVal = `${value[0]}${value[1].props.children}${value[2] || ''}`;
+            break;
+          case 2:
+            selectedVal = `${value[0].props.children}${value[1]}${value[2].props.children}`;
+            break;
+          default:
+            selectedVal = `${value[0]}${value[1].props.children}${value[2]}${value[3].props.children}`;
+        }
+      });
+      setSelectedValue(prevValue => {
+        const values = prevValue.split(' ');
+        values.map((item, index) => {
+          item.includes('#') && values.splice(index, 1);
+          return true;
+        });
+        const newValues = values.join('');
+        return `${newValues} #${selectedVal}`
+      });
+    } else {
       setSelectedValue(value);
     }
   }
 
+  const onSend = () => {
+    const data = [
+      {
+        "key": "type",
+        "value": "save",
+        "type": "text"
+      },
+      {
+        "key": "description",
+        "value": selectedValue,
+        "type": "text"
+      }
+    ]
+    const formdata = new FormData(data);
+    api('tag', 'GET', formdata).then(response => {
+      message.success('Saved Successfully!');
+    })
+  }
+
   return (
-    <>    
-    <Row justify='center' className='hashArea__title'>
-      <Col span={4} style={{paddingLeft:45}}>
-        <Title>Hash Select</Title>
-      </Col>
-    </Row>
-    <Row justify='center'>
-      <Col span={4}>
-        <AutoComplete
-          options={options}
-          className='w-100'
-          onSelect={onSelect}
-          onSearch={handleSearch}
-          onChange={onChange}
-          value={selectedValue}
-        >
-          <TextArea
-            placeholder="input here"
-            className="custom"
+    <>
+      <Row justify='center' className='hashArea__title'>
+        <Col span={4} style={{ paddingLeft: 45 }}>
+          <Title>Hash Select</Title>
+        </Col>
+      </Row>
+      <Row justify='center'>
+        <Col span={4}>
+          <AutoComplete
+            options={options}
+            className='w-100'
+            onSelect={onSelect}
+            onSearch={handleSearch}
+            onChange={onChange}
             value={selectedValue}
-          />
-        </AutoComplete>
-      </Col>
-    </Row>
+          >
+            <TextArea
+              placeholder="input here"
+              className="custom"
+              value={selectedValue}
+            />
+          </AutoComplete>
+        </Col>
+      </Row>
 
     </>
   );
